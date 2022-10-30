aede3b251d672ae829ae14fdd7f1d327004682d1
NeonDactyl <treyw05@gmail.com>
2022-10-30 12:59:25 -0500

    moved code from gun into player

--- src/objects/player/player.ts
+++ src/objects/player/player.ts
@@ -11,12 +11,25 @@ export class Player extends Phaser.GameObjects.Container {
   private gun: Gun;
   private hitPoints: number;
   private maxHitPoints: number;
+  public static maxRotation: number = Math.PI / 2;
+  public static minRotation: number = Player.maxRotation * -1;
+  private bullets: Bullet[];
+  private cooldownRemaining: number;
+  private cooldown: number;
+  private damage: number;
+ 
   
   constructor(aParams: IPlayerOptions) {
     super(aParams.scene, aParams.x, aParams.y);
 
     this.hitPoints = 30;
     this.maxHitPoints = 300;
+    this.bullets = [];
+    this.cooldownRemaining = 0;
+
+    this.cooldown = 15;
+    this.damage = 100;
+
 
     this.gun = new Gun ({
       scene: this.scene,
@@ -40,7 +53,48 @@ export class Player extends Phaser.GameObjects.Container {
   }
 
   update(wave: Wave): void {
-    this.gun.update();
+    this.handleInput();
+    this.updateBullets();
+    this.checkEnemyCollisionsWithBase(wave);
+  }
+
+  private handleInput(): void {
+    this.cooldownRemaining = Math.max(0, this.cooldownRemaining - 1);
+    let deltaX = this.scene.input.x - this.x;
+    let deltaY = this.scene.input.y - this.y;
+    let angle = Math.atan2(deltaY, deltaX) + (Math.PI / 2);
+    if (angle >= Player.maxRotation) {
+      angle = (deltaX < 0 ? Player.minRotation : Player.maxRotation);
+    }
+    if (angle <= Player.minRotation) angle = Player.minRotation;
+    this.gun.setRotation(angle - (Math.PI / 2));
+
+    if (this.scene.input.activePointer.primaryDown && this.cooldownRemaining == 0) {
+      this.shoot();
+    }
+  }
+
+  private shoot(): void {
+    let r = this.gun.rotation + Math.PI / 2;
+    console.log('shooting');
+    let offset = 0.7;
+    this.bullets.push(
+      new Bullet({
+        scene: this.scene,
+        rotation: this.gun.rotation,
+        options: {
+          x: this.x + (this.gun.width * Math.sin(r) * offset),
+          y: this.y - (this.gun.width * Math.cos(r) * offset)
+        },
+        size: 10,
+        damage: this.damage,
+        speed: 15
+      })
+    );
+    this.cooldownRemaining = this.cooldown;
+  }
+
+  private checkEnemyCollisionsWithBase(wave: Wave) {
     for (let i = 0; i < wave.enemies.length; i++) {
       if (this.scene.physics.collide(this.dome, wave.enemies[i]))
       {
@@ -52,7 +106,7 @@ export class Player extends Phaser.GameObjects.Container {
   }
 
   getBullets(): Bullet[] {
-    return this.gun.getBullets();
+    return this.bullets;
   }
 
   public isDead(): boolean {
@@ -60,7 +114,8 @@ export class Player extends Phaser.GameObjects.Container {
   }
 
   levelUp(): void {
-    this.gun.levelUp();
+    this.cooldown *= 0.9;
+    this.damage *= 1.1;
   }
 
   getHpString(): string {
@@ -70,5 +125,16 @@ export class Player extends Phaser.GameObjects.Container {
   public takeDamage(enemy: Enemy): void{
     this.hitPoints -= enemy.getAttack();
     console.log(`We have ${this.hitPoints} hp left`)
+  }
+
+  private updateBullets(): void {
+    for (let i = 0; i < this.bullets.length; i++) {
+      if (this.bullets[i].active) {
+        this.bullets[i].update();
+      } else {
+        this.bullets[i].destroy();
+        this.bullets.splice(i, 1);
+      }
+    }
   }
 }
